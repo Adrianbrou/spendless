f71d9a86feedd1c2501a13dc80ea449f5fcd677b
Adrianbrou <adrian690@hotmail.fr>
2026-05-20 13:03:31 -0400

    add: first API route for testing

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Spendless API is running"}
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
